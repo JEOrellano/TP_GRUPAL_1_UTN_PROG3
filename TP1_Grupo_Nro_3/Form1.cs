5781b18a719701c0100303909718af3385f6a6b4
Esteban <mobilelegendsbang2020bang@gmail.com>
2023-03-29 08:36:15 -0300

    probando gitignore para evitar conflictos en avances

---
+++ TP1_Grupo_Nro_3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_Grupo_Nro_3
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjercicio1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEjercicio2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEjercicio3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
